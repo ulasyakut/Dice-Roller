457984425396cb4407840054f83bd310dfe1dd1a
ulasyakut <ulasyakut1@gmail.com>
2024-11-11 08:53:04 -0500

    done

--- roll.py
+++ roll.py
@@ -1,7 +1,25 @@
+import random
+
 # roll a "die" some number of times.
 # roll - run it once and go into a loop
 # roll 1 - produce a number 1-6 random and print it
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+
+
+def roll():
+    print(random.randint(1,6))
+
+def roll1():
+   roll()
+
+def roll2():
+    roll()
+    roll()
+
+def roll3():
+    roll()
+    roll()
+    roll()
 
